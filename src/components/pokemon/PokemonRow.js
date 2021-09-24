eb6ec2797f740fd4882d87f078f96d9e3f189c11
Manuel Brestak <manuel.brestak@gmail.com>
2021-09-24 15:45:11 +0200

    Added sprites

--- src/components/pokemon/PokemonRow.js
+++ src/components/pokemon/PokemonRow.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const PokemonRow = ({ pokemon }) => {
+const PokemonRow = ({ pokemon, id }) => {
+  const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   return (
     <div className="col-lg-3 col-md-4 col-sm-6">
       <div className="card text-center">
-        <div className="card-body">
-          <h5 className="card-title capitalize">{pokemon.name}</h5>
+        <img src={imgSrc} className="card-img-top pokemon-sprite" alt="" />
+        <div className="card-body pt-0">
+          <h5 className="card-title">{pokemon.name}</h5>
           <a href="#" className="btn btn-primary">
             View More
           </a>
--- src/components/pokemon/PokemonsTable.js
+++ src/components/pokemon/PokemonsTable.js
@@ -4,16 +4,18 @@ import PokemonRow from './PokemonRow';
 const PokemonsTable = ({ pokemons, filterText }) => {
   const rows = [];
 
-  pokemons.forEach(pokemon => {
+  pokemons.forEach((pokemon, index) => {
     if (pokemon.name.indexOf(filterText.toLowerCase()) === -1) {
       return;
     }
-    rows.push(<PokemonRow pokemon={pokemon} key={pokemon.name} />);
+    rows.push(
+      <PokemonRow pokemon={pokemon} key={pokemon.name} id={index + 1} />
+    );
   });
 
   return (
     <div className="container mt-5">
-      <div className="row g-4">{rows}</div>
+      <div className="row g-4 capitalize">{rows}</div>
     </div>
   );
 };
